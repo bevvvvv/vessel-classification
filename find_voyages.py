0394e120f77085a7d4d81d9b1f90b096eccdbe94
Joseph Sepich <computerwhiz1337@gmail.com>
2020-11-08 15:32:34 -0500

    test script

---
+++ find_voyages.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from voyage_utils import voyage_finder, run_vf, assign_id, remove_dupes
+
+#######################################################################
+######### READ DATA
+#######################################################################
+
+print('Reading data frame...')
+df = pd.read_csv('./data/AIS/AIS_2019_01_01.csv')
+num_points = df.shape[0]
+num_ships = len(df.MMSI.unique())
+
+#######################################################################
+######### CONVERT DATE TO EPOCH
+#######################################################################
+
+print('Converting date timestamp...')
+df['BaseDateTime'] = pd.to_datetime(df['BaseDateTime'], format="%Y-%m-%dT%H:%M:S")
+
+#######################################################################
+######### RUN VF METHODS
+#######################################################################
+
+print('Running voyage_finder...')
+df = remove_dupes(df)
+df = run_vf(df)
+df = assign_id(df)
+print('Found voyages!')
+
+#######################################################################
+######### WRITE TO NEW FILE
+#######################################################################
+
+df.to_csv('./data/AIS_voyage_01_01.csv')
+
+print('Point difference: ', df.shape[0] - num_points)
+print('Ship difference: ', len(df.MMSI.unique()) - num_ships)
+print('Voyages found: ', len(df.voyage_id.unique()))
